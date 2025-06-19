910af4a10ef6d67268e152dff519f638d0faf116
sruthiraman95Venkat <sruthiraman95@gmail.com>
2025-06-19 18:54:59 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+import os
+app = Flask(__name__)
+@app.route("/")
+def hello():
+    return "<h1>Welcome to Docker World !!!!</h1>"
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 80))
+    app.run(debug=True,host='0.0.0.0',port=port)
+ 
